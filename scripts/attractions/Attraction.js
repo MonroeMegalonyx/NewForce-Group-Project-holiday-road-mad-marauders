180fe9cecd25fa6e7ccac32bcef56d8643090da9
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-03 16:03:10 -0500

    i think it finally works

--- scripts/attractions/Attraction.js
+++ scripts/attractions/Attraction.js
@@ -2,7 +2,6 @@ export function Attraction(attractionObject){
   return `
   <section class="attraction-card">
     <h2>${attractionObject.name}</h2>
-    <p>${attractionObject.id}</p>
     <button class="details-button" id="details--${attractionObject.id}">Get Details</button>
       
   </section>`
--- scripts/attractions/AttractionsSelect.js
+++ scripts/attractions/AttractionsSelect.js
@@ -4,17 +4,22 @@ import {Attraction} from "./Attraction.js"
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".attractions-dropdown")
 
+// export the function that puts it all together
 export const AttractionsSelect = () => {
   // we're calling that function that returns the info from the api and THEN more stuff
   getAttractions().then(() => {
-     
+      // call the function that is a copy of the array and store it in the attractions variable
       const attractions = useAttractions();
-       render(attractions)
+      // call the render function that we're about to make and pass in attractions as the argument
+      render(attractions)
   
   })
 }
 
+// creating a render function and setting a parameter where we can pass in the attractions array
 function render(attractionsCollection){
+  // contentTarget holds a reference to the attractions-dropdown from our HTML
+  // we're creating a dropdown with select, adding a default option with option, then whatever argument we use when we call the function, we're using .map to map over it, we set a parameter called attractionObject, then we declare a variable called attractionName and we store the value of name for each attraction that gets mapped over inside that variable, then we return some html that makes option tags with the individual attraction names inside 
   contentTarget.innerHTML = `
   <select class="attraction-dropdown" id="attractionSelect">
   <option>Please select an attraction...</option>
@@ -28,19 +33,19 @@ function render(attractionsCollection){
   `
 }
 
+// get a reference to the attractions-card HTML and store it in attractionContainer
 const attractionContainer = document.querySelector(".attractions-card");
 // select the main element in the html and store it in eventHub
 const eventHub = document.querySelector("main")
 // listen to main for a changeEvent, which is associated with a dropdown selection, make a parameter called eventObject for this upcoming function
 eventHub.addEventListener("change", (eventObject) => {
     
-    // if the change happens in the dropdown with the id of crimeSelect(the crime filter dropdown)
+    // if the change happens in the dropdown with the id of attractionSelect
     if(eventObject.target.id === "attractionSelect"){
-       
-      // Make a new object representation of a note
+      // we're calling the useAttractions function, using the .find method on that collection of arrays, we're pulling out x if what the user clicked on has the same value as x.name. x is each individual attraction, then we're putting all those names in a variable called newAttraction
       let newAttraction = useAttractions().find(x => eventObject.target.value === x.name)
       
-    
+      // we're calling the Attraction function that builds an HTML representation and we're passing in the argument of newAttraction that has the name of the attraction that matches the one the user clicked on
       let attractionHTML = (Attraction(newAttraction));
       attractionContainer.innerHTML = attractionHTML;
     }
@@ -62,30 +67,37 @@ eventArea.addEventListener("click", (eventObject) => {
   getAttractions().then(() => {
   let attraction = useAttractions();
    
-    // console.log(event.target.id.split("-")[1])
     if (eventObject.target.id.includes("details--")) {
       
       let singleAttraction = attraction.find(attractionInLoop => attractionInLoop.id == eventObject.target.id.split("--")[1])
-      // let associateName = [];
-      // let associateAlibi = [];
-      console.log(attraction)
+      let attractionName = "";
+      let attractionState = "";
+      let attractionCity = "";
+      let attractionDescription = "";
+      let attractionSouvenirs = "";
+      let attractionRestrooms = "";
+
       console.log(eventObject.target.id)
       console.log(eventObject.target.id.split("--"))
       console.log(singleAttraction)
-      for(let i = 0; i < singleAttraction.length; i++) {
         
-        associateName += singleCriminal.known_associates[i].name + " "
-        associateAlibi += singleCriminal.known_associates[i].alibi + " "
+        attractionName += singleAttraction.name 
+        attractionState += singleAttraction.state
+        attractionCity += singleAttraction.city
+        attractionDescription += singleAttraction.description
+        attractionSouvenirs += singleAttraction.ameneties.souvenirs
+        attractionRestrooms += singleAttraction.ameneties.restrooms
        
         detailsBox.innerHTML = `
         <div class="details-container">
-          State: ${associateName}
-          City: ${associateAlibi}
-          Description: ${associateAlibi}
-          Ameneties: ${associateAlibi}
+          <h3>Name:</h3> <p>${attractionName}</p>
+          <h3>State:</h3> <p>${attractionState}</p>
+          <h3>City:</h3> <p>${attractionCity}</p>
+          <h3>Description:</h3> <p>${attractionDescription}</p>
+          <h3>Souvenirs:</h3> <p>${attractionSouvenirs}</p>
+          <h3>Restrooms:</h3> <p>${attractionRestrooms}</p>
         </div>
-      `;
-      }
+      `
     }
   })
 })   
