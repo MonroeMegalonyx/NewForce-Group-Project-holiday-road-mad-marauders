99dbd8e80b3e4242c48b906fac4b56553622d35f
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-05 10:23:40 -0500

    refactored attractions javascript for separation of concerns

--- scripts/attractions/Attraction.js
+++ scripts/attractions/Attraction.js
@@ -1,7 +1,18 @@
 export function Attraction(attractionObject){
   return `
-  <section class="attraction-card">
+  
     <h2>${attractionObject.name}</h2>
     <button class="details-button" id="details--${attractionObject.id}">Get Details</button>
-  </section>`
+  `
+}
+
+export function AttractionDetails(selectedAttraction) {
+  return `
+  <div class="details-container">
+    <h3>Name:</h3> <p>${selectedAttraction.name}</p>
+    <h3>State:</h3> <p>${selectedAttraction.state}</p>
+    <h3>City:</h3> <p>${selectedAttraction.city}</p>
+    <h3>Description:</h3> <p>${selectedAttraction.description}</p>
+  </div>
+  `
 }
