fe07ad07c70bd11dc18c810e93d82fbe20ffd03f
Detective-Coder <aaronfrankenfield@gmail.com>
2021-02-08 07:41:18 -0500

    trying to get the dang stretch goal

--- scripts/itineraries/ItineraryString.js
+++ scripts/itineraries/ItineraryString.js
@@ -1,4 +1,4 @@
-import {saveItinerary} from "./ItineraryDataProvider.js"
+import {getItineraries, saveItinerary} from "./ItineraryDataProvider.js"
 import {Itinerary} from "./Itinerary.js"
 
 // getting a reference to the main element
@@ -20,7 +20,7 @@ eventHub.addEventListener("click", clickEvent => {
 
       // Change API state and application state, we're calling the saveNote function - which takes in the info from the argument of newNote and writes it to the local api, then we're calling the NoteList function, which I believe just gets everything again
       saveItinerary(newItinerary)
-      // .then(NoteList) // Refresh your list of notes once you've saved your new one, not sure the equivalent of this step
+      // .then(getItineraries) // Refresh your list of notes once you've saved your new one, not sure the equivalent of this step
       console.log(newItinerary)
   }
 })
